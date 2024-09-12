bd02e428e74efd6132127a579d1e691c3a14bbe6
Douglas Zingg <douglaszingg@gmail.com>
2024-09-12 15:48:29 -0700

    Added Scripts
    Added some basic scripts to the Main Scene. These script are enemy spawner, player movement and enemy movement.

---
+++ IdleTower/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float movementSpeed = 3.0f;
+    public GameObject target;
+    public Vector2 spawnPoint = new Vector2(0, 0);
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+        rb.useFullKinematicContacts = true;
+        target = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, movementSpeed * Time.deltaTime);
+    }
+}
